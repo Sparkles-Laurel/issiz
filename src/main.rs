246fd64f7fad4afbf786566d1baab51bac5e9c3a
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2022-02-19 11:12:43 +0300

    Add a sufficient help message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env::args;
-
+use std::process::exit;
 fn main() {
 
     let args_joined = args().skip(1).collect::<Vec<_>>().join(" ");
     // println!("Joined form: {:?}", args_joined);
     
     let chars = args_joined.chars().collect::<Vec<_>>();
-
+    
+    if chars == vec![] || args().skip(1).any(|arg| arg == "--help") {
+        println!(include_str!("../issiz.help"));
+        exit(1);
+    }
     for i in 0..=(chars.len()) {
         println!("{}",String::from_iter((&chars[0..i]).iter()));
     }
